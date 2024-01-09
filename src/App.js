4687f65b0493f24a3975187d59222ed70342a59c
Joshua Abah <donjoe270@gmail.com>
2024-01-09 08:41:06 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Rephrase from './Components/Rephrase';
+
+function App() {
+  return (
+    <div className="App">
+     <Rephrase />
+    </div>
+    
+  );
+}
+
+export default App;
