401f1ae279650df50a2f073f2f2eb851f190b07b
Joshua Abah <donjoe270@gmail.com>
2024-01-12 01:28:43 +0100

    Routes commit

--- src/App.js
+++ src/App.js
@@ -18,8 +18,33 @@
 
 // export default App;
 
+// import React from 'react';
+// import useFetch from './Components/useFetch';
+
+// const App = () => {
+//   const apiUrl = 'https://jsonplaceholder.typicode.com/todos/1'; 
+//   const { data, loading, error } = useFetch(apiUrl);
+
+//   return (
+//     <div>
+//       <h2>API Data:</h2>
+//       {loading && <p>Loading...</p>}
+//       {error && <p>Error: {error.message}</p>}
+//       {data && (
+//         <div>
+//           <p>Title: {data.title}</p>
+//           <p>Completed: {data.completed ? 'Yes' : 'No'}</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default App;
+
 
 // import React from 'react';
+// import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 // import './App.css';
 // import Josh from './Components/Josh';
 // import Card from './Components/Card';
@@ -38,25 +63,44 @@
 
 // export default App;
 
+
 import React from 'react';
-import useFetch from './Components/useFetch';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
 const App = () => {
-  const apiUrl = 'https://jsonplaceholder.typicode.com/todos/1'; 
-  const { data, loading, error } = useFetch(apiUrl);
-
   return (
-    <div>
-      <h2>API Data:</h2>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
-      {data && (
-        <div>
-          <p>Title: {data.title}</p>
-          <p>Completed: {data.completed ? 'Yes' : 'No'}</p>
-        </div>
-      )}
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/Components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Page</h2>
+      <p>Contact us using the information provided on this page.</p>
+    </div>
+  );
+};
+
+export default Contact;
